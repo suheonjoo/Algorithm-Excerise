6c36344c58953f909fcff3f78584210f6a256403
suheonjoo <suheonj95@naver.com>
2022-02-28 21:18:16 +0900

    220228

--- bjOJ/no_1339.java
+++ bjOJ/no_1339.java
@@ -9,10 +9,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
-
-public class Main { // 테스트 자바임
+public class no_1339 { // 테스트 자바임
 
     public static int[] alpha = new int[26];
+
     public static void main(String[] args) throws NumberFormatException, IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,23 +20,23 @@ public class Main { // 테스트 자바임
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int x=0;x<N;x++){
+        for (int x = 0; x < N; x++) {
             String line = br.readLine();
 
             int size = line.length();
 
-            int base = (int) Math.pow(10,size -1);
+            int base = (int) Math.pow(10, size - 1);
 
-            for(int y=0;y<size;y++){
-                alpha[line.charAt(y)-'A']+=base;
-                base/=10;
+            for (int y = 0; y < size; y++) {
+                alpha[line.charAt(y) - 'A'] += base;
+                base /= 10;
             }
         }
 
         Arrays.sort(alpha);
-        int ans=0;
-        for(int x=25;x>=17;x--){
-            ans+=alpha[x]*(x-16);
+        int ans = 0;
+        for (int x = 25; x >= 17; x--) {
+            ans += alpha[x] * (x - 16);
         }
         System.out.println(ans);
     }
