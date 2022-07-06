a406e30bf8e988670b7629894cb86169d829b344
suheonjoo <suheonj95@naver.com>
2022-07-07 00:48:18 +0900

    220707

--- src/Main.java
+++ src/Main.java
@@ -9,38 +9,20 @@ public class Main { // 테스트 자바임
 
         //StringBuffer sb = new StringBuffer();
 
-        int[] participant = {1, 2, 3, 2, 3};
+        int[] participant = {3, 30, 34,1,2,3};
         //String[] completion = {"eden", "kiki"};
         int[][] clothes = {{0, 3}, {1, 9}, {2, 6}};
 
-//        System.out.println("clothes. = " + Arrays.deepToString(clothes));
-//
-//        Integer[] integerArr = new Integer[] {1,3,5,2,4};
-//        //int[] integerArr = new int[] {1,3,5,2,4};
-//        String[] stringArr = new String[] {"A","C","B","E","D"};
-//
-//        Arrays.sort(integerArr,Comparator.reverseOrder());     //내림차순
-//        System.out.println("integerArr = " + Arrays.toString(integerArr));
-//
-//        Arrays.sort(stringArr,Comparator.reverseOrder());    //내림차순
-//        System.out.println("stringArr = " + Arrays.toString(stringArr));
 
 
-        //System.out.println("s.solution(participant) = " + Solution.solution(participant));
+
+        System.out.println("s.solution(participant) = " + Solution.solution(participant));
         //System.out.println(solution);
 
-        //B b = new B(10);
-        //C c = new C();
-        //Y y = new Y();
-
-        A a = new A();
 
     }
 }
-class A{
 
-
-}
 
 class Solution {
 
@@ -53,7 +35,7 @@ class Solution {
 
 
 
-    public String solution(int[] numbers) {
+    public static String solution(int[] numbers) {
 
         String answer = "";
         String []res =new String[numbers.length];
@@ -63,14 +45,16 @@ class Solution {
             res[i]=String.valueOf(numbers[i]);
 
         }
-        //문자열을 합쳤을 때  비교
+        //문자열을 합쳤을 때  비교 //오름차순임 !!
         Arrays.sort(res, (a,b)->{
+            System.out.println("a = " + a +" b = " + b);
+
                     return (a+b).compareTo(b+a);
                 }
         );
+        System.out.println("res = " + Arrays.toString(res));
 
-        //정렬한 값 더하기
-
+        //정렬한 값 더하기 //지금 보면 정렬한 값을 꺼꾸로 더함
         for(int i=numbers.length-1; i>=0; i--){
             answer+=res[i];
         }
